8d603ee5b9747da2f213b53a28c2a151274bc316
Isha Pawar <ishaapawarr@gmail.com>
2024-10-05 01:27:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,8 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+module.exports = {
+  output: 'export',
+  // If your GitHub Pages site is hosted in a subdirectory, specify the `basePath`
+  // basePath: '/your-repo-name',
+};
